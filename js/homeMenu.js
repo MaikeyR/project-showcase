0c7b60de482d4fd87ad6fae73a66e3f14bec8bd2
MaikeyR <MaikeyR@users.noreply.github.com>
2023-06-18 21:21:45 +0000

    deploy: fcde12feeecbb5728ad248a0aafea299a3beacd2

--- js/homeMenu.js
+++ js/homeMenu.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const menuOptions = document.getElementById('home-menuOptions');
   const menuButton = document.querySelector('.home-menuButton');
   const menuSpan = document.querySelector('.home-menuButton span');
-  const menuImg = document.querySelector('.home-menuButton');
+  const menuImg = document.querySelector('.home-menuLogo');
 
   console.log(menuButton);
   console.log(menuOptions);
