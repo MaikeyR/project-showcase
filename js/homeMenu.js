704786d740dc01d14f6fb6fd0165342c33e00dd1
MaikeyR <MaikeyR@users.noreply.github.com>
2023-06-18 17:01:26 +0000

    deploy: 063c96b422f582b33fae10973ff8f594e5654aa9

--- js/homeMenu.js
+++ js/homeMenu.js
@@ -1,6 +1,28 @@
-const menuButton = document.getElementById('menuButton');
-const menuOptions = document.getElementById('menuOptions');
+document.addEventListener("DOMContentLoaded", function() {
+  const menuOptions = document.getElementById('home-menuOptions');
+  const menuButton = document.querySelector('.home-menuButton');
 
-menuButton.addEventListener('click', () => {
-  menuOptions.style.display = menuOptions.style.display === 'none' ? 'block' : 'none';
+  console.log(menuButton);
+  console.log(menuOptions);
+
+  menuButton.addEventListener("click", function () {
+    console.log("Menu button clicked");
+    menuOptions.classList.toggle("show");
+    if (menuOptions.classList.contains("show")) {
+      menuButton.textContent = "Close";
+    } else {
+      menuButton.textContent = "Menu";
+    }
+  });
+
+  document.addEventListener("click", function (event) {
+    const target = event.target;
+    if (
+      !target.classList.contains("home-menuButton") &&
+      !target.classList.contains("home-menuOptions")
+    ) {
+      menuOptions.classList.remove("show");
+      menuButton.textContent = "Menu";
+    }
+  });
 });
