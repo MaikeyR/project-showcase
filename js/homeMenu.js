d138ee084dd4234e65d8685f64c48dc2cc43f81a
MaikeyR <MaikeyR@users.noreply.github.com>
2023-06-18 21:18:45 +0000

    deploy: 5025be9530063732366b030494810bec23bf26e5

--- js/homeMenu.js
+++ js/homeMenu.js
@@ -1,12 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
   const menuOptions = document.getElementById('home-menuOptions');
   const menuButton = document.querySelector('.home-menuButton');
+  const menuSpan = document.querySelector('.home-menuButton span');
+  const menuImg = document.querySelector('.home-menuButton');
 
   console.log(menuButton);
   console.log(menuOptions);
+  console.log(menuSpan);
+  console.log(menuImg);
 
   menuButton.addEventListener("click", function(event) {
-    if (event.target === menuButton || menuButton.contains(event.target) && !event.target.matches("span, img")) {
+    if (event.target === menuButton) {
+      console.log("Menu button clicked");
+      menuOptions.classList.toggle("show");
+    }
+  });
+  menuSpan.addEventListener("click", function(event) {
+    if (event.target === menuSpan) {
+      console.log("Menu button clicked");
+      menuOptions.classList.toggle("show");
+    }
+  });
+  menuImg.addEventListener("click", function(event) {
+    if (event.target === menuImg) {
       console.log("Menu button clicked");
       menuOptions.classList.toggle("show");
     }
