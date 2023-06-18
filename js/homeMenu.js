944fd41e632ca40e5cdfef259c6fd436fdeb1b94
MaikeyR <MaikeyR@users.noreply.github.com>
2023-06-18 20:48:58 +0000

    deploy: cfcb26ed9b3a871306eaf92e203e0504737be25f

--- js/homeMenu.js
+++ js/homeMenu.js
@@ -9,22 +9,6 @@ document.addEventListener("DOMContentLoaded", function() {
     if (event.target === menuButton || menuButton.contains(event.target) && !event.target.matches("span, img")) {
       console.log("Menu button clicked");
       menuOptions.classList.toggle("show");
-      if (menuOptions.classList.contains("show")) {
-        menuButton.textContent = "Close";
-      } else {
-        menuButton.textContent = "Menu";
-      }
-    }
-  });
-
-  document.addEventListener("click", function (event) {
-    const target = event.target;
-    if (
-      !target.classList.contains("home-menuButton") &&
-      !target.classList.contains("home-menuOptions")
-    ) {
-      menuOptions.classList.remove("show");
-      menuButton.textContent = "Menu";
     }
   });
 });
