afb4a41e90602bfbd4e030a2a26aa48576412315
MaikeyR <MaikeyR@users.noreply.github.com>
2023-06-19 08:58:50 +0000

    deploy: 01cdfac1ea3d62ea6c246ffcf7229e28b615d456

--- js/homeMenu.js
+++ js/homeMenu.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const menuButton = document.querySelector('.home-menuButton');
   const menuSpan = document.querySelector('.home-menuButton span');
   const menuImg = document.querySelector('.home-menuLogo');
+  const menuCross = document.querySelector('.cross');
 
   console.log(menuButton);
   console.log(menuOptions);
@@ -12,6 +13,13 @@ document.addEventListener("DOMContentLoaded", function() {
   menuButton.addEventListener("click", function(event) {
     if (event.target.matches(".home-menuButton, .home-menuButton span, .home-menuButton img")) {
       console.log("Menu button clicked");
+      menuOptions.classList.toggle("show");
+    }
+  });
+
+  menuOptions.addEventListener("click", function(event) {
+    if (event.target.matches(".cross")) {
+      console.log("Cross button clicked");
       menuOptions.classList.toggle("show");
     }
   });
