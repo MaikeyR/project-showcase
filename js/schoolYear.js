d9a036499a3056aadc2cc4a5412a6923344ea808
MaikeyR <MaikeyR@users.noreply.github.com>
2023-07-10 15:01:58 +0000

    deploy: 89e5b451d98a8836e95302a19085bfaf74c55ced

---
+++ js/schoolYear.js
@@ -0,0 +1,13 @@
+const selectSchoolYear = document.getElementById("schoolYear");
+const currentYear = new Date().getFullYear();
+
+for (let i = 0; i < 10; i++) {
+  const startYear = currentYear - i;
+  const endYear = startYear + 1;
+  const schoolYear = `${startYear}-${endYear}`;
+
+  const option = document.createElement("option");
+  option.value = schoolYear;
+  option.text = schoolYear;
+  selectSchoolYear.appendChild(option);
+}
