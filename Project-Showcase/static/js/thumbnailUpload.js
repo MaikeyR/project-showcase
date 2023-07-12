be4f29c39a09e535451749824ed331f7068bb21c
Maikel <mreijneke@student.che.nl>
2023-07-12 21:41:16 +0200

    add project 121

--- Project-Showcase/static/js/thumbnailUpload.js
+++ Project-Showcase/static/js/thumbnailUpload.js
@@ -1,3 +1,12 @@
+// Update the file name display when a file is selected
+const fileInput = document.getElementById('imageUpload');
+const fileNameDisplay = document.querySelector('.file-name');
+
+fileInput.addEventListener('change', () => {
+  const fileName = fileInput.files[0]?.name;
+  fileNameDisplay.textContent = fileName ? `Selected File: ${fileName}` : '';
+});
+
 const imageUpload = document.getElementById('imageUpload');
 imageUpload.addEventListener('change', (event) => {
   const file = event.target.files[0];
