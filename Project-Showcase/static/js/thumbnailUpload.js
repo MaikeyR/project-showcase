f0576dc6e0fe2c71be47c8d845888e872f88d78e
Maikel <mreijneke@student.che.nl>
2023-07-12 20:35:35 +0200

    add project 120

---
+++ Project-Showcase/static/js/thumbnailUpload.js
@@ -0,0 +1,5 @@
+const imageUpload = document.getElementById('imageUpload');
+imageUpload.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  // Handle the selected file here
+});
