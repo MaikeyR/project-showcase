f19b56db52426d5b947b747b307e5f1e213f7d08
Maikel <mreijneke@student.che.nl>
2023-07-26 16:22:17 +0200

    add project 132

--- Project-Showcase/static/js/thumbnailUpload.js
+++ Project-Showcase/static/js/thumbnailUpload.js
@@ -1,10 +1,28 @@
-// Update the file name display when a file is selected
 const fileInput = document.getElementById('imageUpload');
 const customLabel = document.querySelector('.imageUploadLabel');
+const imagePreview = document.querySelector('.imagePreview');
 
-fileInput.addEventListener('change', () => {
-  const fileName = fileInput.files[0]?.name;
-  customLabel.textContent = fileName ? `Selected File: ${fileName}` : 'Kies Bestand';
+fileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const fileName = file.name;
+    customLabel.textContent = fileName;
+    
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const img = document.createElement('img');
+      img.src = e.target.result;
+      img.style.maxWidth = '100px'; // Set the maximum width for the preview image
+      imagePreview.innerHTML = ''; // Clear previous preview
+      imagePreview.appendChild(img); // Add the new preview image
+    };
+
+    reader.readAsDataURL(file);
+  } else {
+    customLabel.textContent = 'Kies Bestand';
+    imagePreview.innerHTML = ''; // Clear the preview when no image is selected
+  }
 });
 
 const imageUpload = document.getElementById('imageUpload');
