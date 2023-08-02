bbcff333e2ee972881e8fb412dae3c27f97f2cd8
Maikel <mreijneke@student.che.nl>
2023-08-02 22:53:53 +0200

    add project 177

--- Project-Showcase/static/js/projectContent.js
+++ Project-Showcase/static/js/projectContent.js
@@ -1,29 +1,80 @@
-const formElementsContainer = document.getElementById('formElements');
-const addElementButtons = document.querySelectorAll('.addElementBtn');
-const submitBtn = document.getElementById('submitBtn');
-
-let elements = []; // Array to store form elements
-
-// Function to create and add a new element to the form
-function addElement(type, content) {
-  const element = document.createElement(type);
-  element.textContent = content;
-  formElementsContainer.appendChild(element);
-  elements.push({ type, content });
+// Function to create and add an input element
+function addInputElement(placeholder, required) {
+  const inputElement = document.createElement('input');
+  inputElement.type = 'text';
+  inputElement.className = 'inputBox';
+  inputElement.placeholder = placeholder;
+  inputElement.required = required;
+  return inputElement;
 }
 
-// Event listener for "Add Element" buttons
-addElementButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const elementType = button.getAttribute('data-type');
+// Function to create and add a textarea element
+function addTextareaElement(placeholder, required) {
+  const textareaElement = document.createElement('textarea');
+  textareaElement.className = 'inputBox';
+  textareaElement.placeholder = placeholder;
+  textareaElement.required = required;
+  return textareaElement;
+}
+
+// Function to create and add an uploaded element (image or audio)
+function addUploadedElement(accept, text) {
+  const uploadedElement = document.createElement('div');
+  uploadedElement.className = 'selectBox uploaded';
+  const fileInput = document.createElement('input');
+  fileInput.type = 'file';
+  fileInput.id = 'fileUpload';
+  fileInput.name = 'fileUpload';
+  fileInput.accept = accept;
+  const label = document.createElement('label');
+  label.htmlFor = 'fileUpload';
+  label.className = 'imageUploadLabel';
+  label.textContent = text;
+  uploadedElement.appendChild(fileInput);
+  uploadedElement.appendChild(label);
+  return uploadedElement;
+}
+
+// Function to create and add an input text element for video embed
+function addVideoInputElement(placeholder) {
+  const inputElement = document.createElement('input');
+  inputElement.type = 'text';
+  inputElement.className = 'inputBox';
+  inputElement.placeholder = placeholder;
+  return inputElement;
+}
+
+// Get the container element that holds the buttons
+const buttonContainer = document.querySelector('.dynamicForm');
+
+// Function to add a new element to the form
+function addElement(type, content) {
+  const formElementsContainer = document.getElementById('formElements');
+
+  if (type === 'title') {
+    formElementsContainer.appendChild(addInputElement('Typ hier de titel van jouw alinea...', true));
+  } else if (type === 'paragraph') {
+    formElementsContainer.appendChild(addTextareaElement('Typ hier de tekst van jouw alinea...', true));
+  } else if (type === 'image') {
+    formElementsContainer.appendChild(addUploadedElement('image/*', '+ Afbeelding'));
+  } else if (type === 'video') {
+    formElementsContainer.appendChild(addVideoInputElement('Typ hierEmbed video code here...'));
+  } else if (type === 'audio') {
+    formElementsContainer.appendChild(addUploadedElement('audio/*', '+ Audio'));
+  } else {
+    const element = document.createElement(type);
+    element.textContent = content;
+    formElementsContainer.appendChild(element);
+  }
+}
+
+// Event listener for all buttons inside the container
+buttonContainer.addEventListener('click', (event) => {
+  if (event.target.classList.contains('dynamicForm')) {
+    const elementType = event.target.getAttribute('data-type');
     const content = ''; // You can get user input for content if needed
     addElement(elementType, content);
-  });
-});
-
-// Event listener for "Submit Form" button
-submitBtn.addEventListener('click', () => {
-  console.log(JSON.stringify(elements)); // You can send this data to the server or use it as needed
+  }
 });
 
 // Optional: Implement reordering and delete functionality using libraries like jQuery UI's Sortable or HTML5's Drag and Drop API.
