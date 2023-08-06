cfda9cc976fa2fd36a23580e909ceb57235c796d
Maikel <mreijneke@student.che.nl>
2023-08-06 16:52:25 +0200

    Revert "add project 238"
    This reverts commit 9f4746f96da36191a7e0645d9a8fb83ec22829e0.

--- Project-Showcase/static/js/addProject.js
+++ Project-Showcase/static/js/addProject.js
@@ -1,6 +1,6 @@
 const studentAccountName = "Maikel Reijneke"; // This should eventually be linked to CHE account logins. For linking people to added projects and managing "Mijn Projecten"
 
-const accessToken = ""; // personal accesstoken from the owner of the repository (Default token from "MaikeyR" expires on Wed, Sep 6 2023)
+const accessToken = process.env.ACCESS_TOKEN; // personal accesstoken from the owner of the repository (Default token from "MaikeyR" expires on Wed, Sep 6 2023)
 
 // Event handler for form submission
 function handleFormSubmit(event) {
